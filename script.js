f088932c41f558ac795bc11231291f5ff4beec84
Maissa <160016532+maissadahmani@users.noreply.github.com>
2024-03-15 21:31:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let popup = document.getElementById("popup");
 
-function showPopup(){
-    popup.style.display="flex";
-}
+
 function closePopup(){
     popup.style.display="none";
 }
+
+document.querySelectorAll("#aufgabe1, #aufgabe2, #aufgabe3, #aufgabe4, #aufgabe5, #aufgabe6, #aufgabe7, #aufgabe8").forEach((ele) => {
+    ele.addEventListener("click", () => {
+        popup.style.display="flex";
+        ele.style.backgroundColor = "#efefef";
+    ele.style.cursor= "not-allowed";
+        
+    });
+});
